97caa221866a7674575c747b9cb5c0985635bd33
Pavel Kim <hello@pavelkim.com>
2022-10-31 22:31:02 +0100

    [Change] Pointing to the obsolete base url

--- pymegafon/common.py
+++ pymegafon/common.py
@@ -3,7 +3,7 @@
     Sets common variables and such.
 """
 
-LK_START_URL = 'https://lk.megafon.ru/login/'
-LK_LOGIN_URL = 'https://lk.megafon.ru/dologin/'
-API_GET_BALANCE_URL = 'https://lk.megafon.ru/api/lk/balance/get'
-API_GET_INTERNET_REMININGS_URL = 'https://lk.megafon.ru/api/options/remaindersMini'
+LK_START_URL = 'https://old.lk.megafon.ru/login/'
+LK_LOGIN_URL = 'https://old.lk.megafon.ru/dologin/'
+API_GET_BALANCE_URL = 'https://old.lk.megafon.ru/api/lk/balance/get'
+API_GET_INTERNET_REMININGS_URL = 'https://old.lk.megafon.ru/api/options/remaindersMini'
